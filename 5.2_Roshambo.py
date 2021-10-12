923015b365ce92de45cb39e48abbe52614b327c8
Marc Hermon <hermonm@urbandaleschools.com>
2021-10-12 09:47:56 -0500

    Update 5.2_Roshambo.py

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -2,12 +2,13 @@
 ROSHAMBO PROGRAM
 ----------------
 
-Create a program that randomly prints 1, 2, or 3.
+Create a program that randomly chooses a 1, 2, or 3.
 Expand the program so it randomly prints rock, paper, or scissors using if statements. Don't select from a list.
-Add to the program so it first asks the user their choice as well as if they want to quit.
-(It will be easier if you have them enter 1 for rock, 2 for paper, and 3 for scissors.)
+Add to the program so it first asks the user their choice as well as if they want to quit.(1 for rock, 2 for paper, 3 for scissors and 4 for quit)
+I don't want to be asked to quit each time. I will enter a 4 if I want to quit.
 Add conditional statements to figure out who wins and keep the records
-When the user quits print a win/loss record
+Each round tell me what the computer chose, what I chose and also if I won, lost or tied.
+When the user quits print an end game message and their win/loss/tie record
 
 '''
 
